94f57808b94faa1688515dcf599d030b13465410
Rutuja <rutujapise28@gmail.com>
2025-07-28 10:16:04 +0530

    Javascript Exam

--- Script.js
+++ Script.js
@@ -4,7 +4,7 @@ function filterAdults(users) {
   return users.filter(user => user.age > 18);
 }
 
-// Example:
+
 const users = [
   { name: "Alice", age: 22 },
   { name: "Bob", age: 17 },
@@ -18,7 +18,6 @@ function sortProductsByPrice(products) {
   return products.sort((a, b) => a.price - b.price);
 }
 
-// Example:
 const products = [
   { name: "Keyboard", price: 499 },
   { name: "Monitor", price: 8999 },
@@ -28,28 +27,23 @@ const products = [
 console.log(sortProductsByPrice(products));
 
 // Assignment no 5
-function slugify(text) {
+function removeSpecialChar(text) {
   return text
-    .toLowerCase()
+     .toLowerCase()
     .replace(/[^\w\s-]/g, '')  // remove special chars
     .trim()
     .replace(/\s+/g, '-');     // replace spaces with hyphens
 }
-
-// Example:
-console.log(slugify("Learn    JavaScript in 30 Days!"));
+console.log(removeSpecialChar("Learn @   JavaScript in 30 Days!"));
 //Assignment no 6
 function groupByLength(words) {
-  return words.reduce((acc, word) => {
+  return words.reduce((a, word) => {
     const len = word.length;
-    acc[len] = acc[len] || [];
-    acc[len].push(word);
-    return acc;
+    a[len] = a[len] || [];
+    a[len].push(word);
+    return a;
   }, {});
 }
-
-// Example:
 console.log(groupByLength(["dog", "apple", "sun", "table", "cat", "pie"]));
-// Output: { 3: ["dog", "sun", "cat", "pie"], 5: ["apple", "table"] }
 
 
